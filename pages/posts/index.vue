1f282a07bc572fbe3760834b8637953175e28864
moriyamataku <moriyamataku2@gmail.com>
2020-07-16 09:41:22 +0900

    refactoring

--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -10,15 +10,15 @@
   </section>
 </template>
 <script>
-import { createClient } from '~/plugins/contentful.js'
+import { createClient, contentTypes } from '~/plugins/contentful.js'
 
 const client = createClient()
+const types = contentTypes()
 export default {
   async asyncData({ params }) {
     // 記事一覧を取得
     const entries = await client.getEntries({
-      // content_type: process.env.CTFL_CONTENT_TYPE_POST,
-      content_type: 'post',
+      content_type: types.post,
     })
     return {
       posts: entries.items,
--- plugins/contentful.js
+++ plugins/contentful.js
@@ -3,10 +3,14 @@ const envSet = require(`~/env.${process.env.NODE_ENV || 'development'}.js`)
 
 module.exports = {
   createClient() {
-    // debugger;
     return contentful.createClient({
       space: envSet.CTFL_SPACE,
       accessToken: envSet.CTFL_ACCESS_TOKEN,
     })
+  },
+  contentTypes() {
+    return {
+      post: envSet.CTFL_CONTENT_TYPE_POST,
+    }
   },
 }
